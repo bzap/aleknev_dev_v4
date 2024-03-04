694f82e9832eecedccf644d24470af4f89b32d98
bzap <linasalekne@gmail.com>
2024-03-03 19:51:04 -0500

    removed hover bg on mobile view

--- src/app/components/projects/ProjectCard.tsx
+++ src/app/components/projects/ProjectCard.tsx
@@ -10,7 +10,7 @@ const ProjectCard = ({ name, desc, link }: ProjectProp) => {
     return (
         <div className="group cursor-pointer justify-center items-center -mx-4">
             <a href={link} target="_blank">
-                <div className="text-zinc-200 text-sm group hover:bg-zinc-800 active:bg-zinc-900 transition rounded-lg py-3 w-full flex justify-between items-center pl-4 pr-3">
+                <div className="text-zinc-200 text-sm group md:hover:bg-zinc-800 active:bg-zinc-900 transition rounded-lg py-3 w-full flex justify-between items-center pl-4 pr-3">
                     <div className="flex flex-col">
                         <div className="font-bold">{name}</div>
                         <div className="text-zinc-400">{desc}</div>
