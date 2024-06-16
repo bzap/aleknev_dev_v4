d2633c65d32744180f6a8799eabc285ba215f5f2
bzap <linasalekne@gmail.com>
2024-06-16 18:03:44 -0400

    added lines next to experience

--- src/app/components/experience/ExperienceCard.tsx
+++ src/app/components/experience/ExperienceCard.tsx
@@ -14,7 +14,7 @@ const ExperienceCard = ({
     link,
 }: ExperienceProp) => {
     return (
-        <div className="group cursor-pointer justify-center items-center -mx-4">
+        <div className="group cursor-pointer justify-center items-center -mx-4 w-full">
             <a href={link} target="_blank">
                 <div className="text-zinc-200 text-sm group md:hover:bg-zinc-800 active:bg-zinc-900 transition rounded-lg py-3 w-full flex justify-between items-center pl-4 pr-3">
                     <div className="flex flex-col">
