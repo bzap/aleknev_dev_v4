c24f31416eac366d4e30751c8b263c1495b04b66
bzap <linasalekne@gmail.com>
2024-06-16 20:52:31 -0400

    added scale animation to timeline dots

--- src/app/components/experience/Experience.tsx
+++ src/app/components/experience/Experience.tsx
@@ -10,9 +10,9 @@ const Experience = () => {
             <div className="pt-2 flex justify-center w-full flex-col">
                 {experiences.map((item, index) => {
                     return (
-                        <div className="flex flex-row gap-8">
+                        <div className="flex flex-row gap-8 group">
                             <div className="mt-[15px]">
-                                <div className="w-3 h-3 bg-zinc-400 dark:bg-zinc-500 rounded-full" />
+                                <div className="w-3 h-3 bg-zinc-400 dark:bg-zinc-500 rounded-full group-hover:scale-125 transition duration-400" />
                                 {index < Object.keys(experiences).length - 1 ? (
                                     <div className="h-[110%] w-2 border-l-[4px] border-zinc-300 dark:border-zinc-700 ml-1" />
                                 ) : (
--- src/app/components/experience/ExperienceCard.tsx
+++ src/app/components/experience/ExperienceCard.tsx
@@ -16,7 +16,7 @@ const ExperienceCard = ({
     return (
         <div className="group cursor-pointer justify-center items-center -mx-4 w-full">
             <a href={link} target="_blank">
-                <div className="text-zinc-500 dark:text-zinc-200 text-sm group md:hover:bg-zinc-200 md:active:bg-zinc-300 dark:md:hover:bg-zinc-800 dark:md:active:bg-zinc-900 transition rounded-lg py-3 w-full flex justify-between items-center pl-4 pr-3">
+                <div className="text-zinc-500 dark:text-zinc-200 text-sm md:hover:bg-zinc-200 md:active:bg-zinc-300 dark:md:hover:bg-zinc-800 dark:md:active:bg-zinc-900 transition rounded-lg py-3 w-full flex justify-between items-center pl-4 pr-3">
                     <div className="flex flex-col">
                         <div className="font-bold">{workplace}</div>
                         <div className="text-zinc-400 dark:text-zinc-400 text-sm -mt-0.5">
